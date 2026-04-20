1b4ffd3f9572f5698a2bf1f2f650f22cd253e92b
lqmanh1612 <quocmanhle0@gmail.com>
2026-04-21 06:01:03 +0700

    Update project: refine visuals, fix mechanics, and update gitignore

--- Assets/Scripts/HomeUI.cs
+++ Assets/Scripts/HomeUI.cs
@@ -8,6 +8,7 @@ public class HomeUI : MonoBehaviour
     [Header("UI References")]
     public TextMeshProUGUI textLevel;
     public Button buttonPlay;
+    public Button buttonReset;
 
     private void Start()
     {
@@ -21,20 +22,22 @@ public class HomeUI : MonoBehaviour
             }
         }
 
-        // Tự động tìm và gán chức năng cho ButtonPlay nếu chưa gán
+        // Tự động tìm và gán chức năng cho các Button nếu chưa gán
         if (buttonPlay == null)
         {
-            GameObject playBtnObj = GameObject.Find("ButtonPlay");
-            if (playBtnObj != null)
-            {
-                buttonPlay = playBtnObj.GetComponent<Button>();
-            }
+            GameObject obj = GameObject.Find("ButtonPlay");
+            if (obj != null) buttonPlay = obj.GetComponent<Button>();
         }
 
-        if (buttonPlay != null)
+        if (buttonReset == null)
         {
-            buttonPlay.onClick.AddListener(LoadGameplayScene);
+            GameObject obj = GameObject.Find("ButtonReset");
+            if (obj != null) buttonReset = obj.GetComponent<Button>();
         }
+
+        // Đăng ký sự kiện
+        if (buttonPlay != null) buttonPlay.onClick.AddListener(LoadGameplayScene);
+        if (buttonReset != null) buttonReset.onClick.AddListener(ResetProgress);
 
         UpdateLevelDisplay();
     }
@@ -42,6 +45,16 @@ public class HomeUI : MonoBehaviour
     private void LoadGameplayScene()
     {
         SceneManager.LoadScene("GameplayScene");
+    }
+
+    private void ResetProgress()
+    {
+        // Xóa tiến trình về Level 1
+        PlayerPrefs.SetInt("CurrentLevelIndex", 0);
+        PlayerPrefs.Save();
+        
+        Debug.Log("Progress Reset to Level 1");
+        UpdateLevelDisplay();
     }
 
     private void UpdateLevelDisplay()
